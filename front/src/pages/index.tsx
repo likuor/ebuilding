e27810bcee35a6134d02f96bd38414043519f8a7
likuor <likuor0528@gmail.com>
2023-05-05 12:19:51 -0700

    #2 add: react query. SSR

--- front/src/pages/index.tsx
+++ front/src/pages/index.tsx
@@ -1,18 +1,43 @@
 import { Inter } from 'next/font/google'
 import { fetchHouses } from './api/api';
+import { useQuery, useMutation } from "@tanstack/react-query";
 
 const inter = Inter({ subsets: ['latin'] })
 
-export const getStaticProps = async () => {
+// type house = {
+//   name: string,
+//   description: string,
+//   price: number,
+// }[]
+
+// export const getStaticProps = async () => {
+//   const houses = await fetchHouses();
+//   return {
+//     props: {
+//       houses
+//     },
+//   }
+// }
+
+// SSR
+export const getServerSideProps = async () => {
   const houses = await fetchHouses();
   return {
     props: {
       houses
     },
   }
-}
+};
 
 export default function Home({ houses }: any) {
+  const houseQuery = useQuery({
+    queryKey: ['house'],
+    queryFn: () => [...houses]
+  })
+
+  if (houseQuery.isLoading) return '...Loading'
+  if (houseQuery.isError) return <pre>{JSON.stringify(houseQuery.error)}</pre>
+
   return (
     <main
       className={`flex min-h-screen items-center justify-between p-24  bg-slate-500${inter.className}`}
