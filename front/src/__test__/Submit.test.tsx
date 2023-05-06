fddfe95b7f4c66bf506a0c10a36f49e1f87a3ad5
likuor <likuor0528@gmail.com>
2023-05-06 16:35:50 -0700

    #7 add: base of test

---
+++ front/src/__test__/Submit.test.tsx
@@ -0,0 +1,10 @@
+// import { render, fireEvent } from '@testing-library/react'
+
+// test('onClick', () => {
+//   const handleClick = jest.fn()
+//   const { getByText } = render(
+//     <Button onClick={() => handleClick()}>hoge</Button>,
+//   )
+//   fireEvent.click(getByText('hoge'))
+//   expect(handleClick).toHaveBeenCalledTimes(1)
+// })
